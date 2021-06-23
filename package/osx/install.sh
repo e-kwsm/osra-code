693ee0cffa09bfb25fcc7775ceecba14e27cd7b3
igor_filippov <igor_filippov@049e15af-3938-0410-b28b-82a5db9f179f>
2021-06-23 21:06:40 +0000

    Package version specified once only in configure.ac and populated from templates.
    git-svn-id: https://svn.code.sf.net/p/osra/code/trunk@1066 049e15af-3938-0410-b28b-82a5db9f179f

--- package/osx/install.sh
+++ package/osx/install.sh
@@ -4,7 +4,7 @@ if (( $EUID != 0 )); then
    exit 1
 fi
 mkdir -p /opt/local/osra/2.1.1 || { echo "Cannot create /opt/local/osra folder" 1>&2; exit; } 
-mkdir -p /usr/local/bin || { echo "Cannot create /opt/local/osra folder" 1>&2; exit; }
+mkdir -p /usr/local/bin || { echo "Cannot create /usr/local/bin folder" 1>&2; exit; }
 cp -f package/* /opt/local/osra/2.1.1/ || { echo "Cannot copy to /opt/local/osra folder" 1>&2; exit; }
 echo "Installing binary files in /opt/local/osra"
 cp -f osra /usr/local/bin || { echo "Cannot copy to /usr/local/bin" 1>&2; exit; }
