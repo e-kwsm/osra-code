d05956e171b2c7f6674d41b7313d5168535ae2a0
igor_filippov <igor_filippov@049e15af-3938-0410-b28b-82a5db9f179f>
2022-12-22 23:38:35 +0000

    Pick up best resolution per structure, not per page
    git-svn-id: https://svn.code.sf.net/p/osra/code/trunk@1086 049e15af-3938-0410-b28b-82a5db9f179f

--- src/osra.h
+++ src/osra.h
@@ -182,6 +182,7 @@ typedef struct bond_s bond_t;
 #define SPELLING_TXT "spelling.txt"
 #define SUPERATOM_TXT "superatom.txt"
 #define RECOGNIZED_CHARS "oOcCnNHFsSBuUgMeEXYZRPp23456789AmThDGQ"
+#define MAX_MOLS_PER_PAGE 1000
 
 #define ERROR_SPELLING_FILE_IS_MISSING          -1
 #define ERROR_SUPERATOM_FILE_IS_MISSING         -2
